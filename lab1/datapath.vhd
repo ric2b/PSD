4ba84ee19b88091942f125cda0a78581c454c26f
davidfialho <fialho.david@gmail.com>
2014-10-09 18:49:12 +0100

    Primeira tentativa de implementação da multiplicação. Não testada! A multiplicação resulta num sinal de 20 bits, os 7 bits mais significativos do resultado são desprezados de forma a obtermos um sinal com 13 bits. É activada a saida 'overflow' caso este ocorra.

--- lab1/datapath.vhd
+++ lab1/datapath.vhd
@@ -24,10 +24,11 @@ use IEEE.STD_LOGIC_SIGNED.ALL;
 
 entity datapath is
     Port ( data_in : in  STD_LOGIC_VECTOR (6 downto 0);
-			  reg_select : in std_logic;
+			  reg_select : in STD_LOGIC;
 			  oper : in STD_LOGIC_VECTOR (1 downto 0);
 			  clk, rst : in STD_LOGIC;
-           data_out : out  STD_LOGIC_VECTOR (12 downto 0));
+           data_out : out  STD_LOGIC_VECTOR (12 downto 0);
+			  overflow : out STD_LOGIC);
 end datapath;
 
 architecture Behavioral of datapath is
@@ -36,8 +37,8 @@ architecture Behavioral of datapath is
 	signal en1, en2, muxSel : std_logic := '0';
 	signal alu : std_logic_vector (12 downto 0) := (others => '0');
 	signal sra_result, sra_result0, sra_result1 : std_logic_vector (12 downto 0) := (others => '0');
-	signal remainder : std_logic_vector (2 downto 0) := (others => '0');
-	-- signal mul : std_logic_vector (19 downto 0) := (others => '0');  -- 13 + 7 bits --
+	signal mul : std_logic_vector (19 downto 0) := (others => '0');  -- 13 + 7 bits --
+	signal result_neg, result_pos : std_logic := '0';
 begin
 	
 	-- Register 1 --
@@ -52,21 +53,11 @@ begin
 			end if;
 		end if;
 	end process;
-	
-	--shift_ra: process
-	--begin
-	--	for i in 0 to 7 loop
-	--		if i <= remainder then
-	--			sra_result <= reg2(12)&reg2(12 downto 1);
-	--		end if;
-	--	end loop;
-	--end process;
+	fullReg1 <= "111111"&reg1 when reg1(6)='1' else "000000"&reg1;
 	
 	-- ALU --
-	remainder <= reg1(2 downto 0);
-	
 	-- SRA --
-	with remainder select
+	with reg1(2 downto 0) select
 		sra_result1 <= "1"&reg2(12 downto 1) when "001",
 			  			   "11"&reg2(12 downto 2) when "010",
 						   "111"&reg2(12 downto 3) when "011",
@@ -76,7 +67,7 @@ begin
 						   "1111111"&reg2(12 downto 7) when "111",
 						   reg2 when others;
 							
-	with remainder select
+	with reg1(2 downto 0) select
 		sra_result0 <= "0"&reg2(12 downto 1) when "001",
 							"00"&reg2(12 downto 2) when "010",
 							"000"&reg2(12 downto 3) when "011",
@@ -88,13 +79,16 @@ begin
 	
 	sra_result <= sra_result0 when reg2(12)='0' else sra_result1;
 	
-	fullReg1 <= "111111"&reg1 when reg1(6)='1' else "000000"&reg1;
-	-- mul <= reg2 * reg1 when data_in(2 downto 0)="011";
+	-- Multiplication --
+	mul <= reg2 * reg1 when data_in(2 downto 0)="011";
+	result_neg <= '1' when (mul(18 downto 12) and "1111111")="1111111" else '0';
+	result_pos <= '1' when (mul(18 downto 12) or "0000000")="0000000" else '0';
+	overflow <= (mul(19) and result_neg) or ((not mul(19)) and result_pos);
+	
 	with data_in(2 downto 0) select
 		alu <= reg2 + fullReg1 when "001",
-				 reg2 - fullReg1 when "010",		-- mudar para usar o somador como subtrator e somador
-				 -- mul(12 downto 0) when "011", -- esta solucao nao permite converter 
-															--	os valores correctamente quando temos overflow --
+				 reg2 - fullReg1 when "010",
+				 mul(12 downto 0) when "011",
 				 reg2 xor fullReg1 when "100",
 				 sra_result when "101",
 				 reg2 when others;	 
