b5d4ee3027166dbf6d5539730825c38069ad67da
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2014-12-16 01:41:45 +0000

    Implementação em python para converter pbm para ficheiro binário puro, incluindo multiplos de uma vez

---
+++ lab3/bitmap-generator/python implementation/converter.py
@@ -0,0 +1,26 @@
+import sys
+ignoreFirst = 50
+
+def check_args():
+	""" checks if a file was supplied to the script """
+	if len(sys.argv) != 2:
+		print "need the filename, dumb dumb"
+		exit()
+	else: 
+		return sys.argv[1]
+
+
+filename = check_args()
+
+try:
+	f = open(filename, 'r')
+except IOError:
+	print "Failed to open '" + filename
+	exit()
+
+bytes_read = open(filename, "rb").read()
+newFile = open(filename + ".bit", "wb")
+newFile.write(bytes_read[ignoreFirst:])
+newFile.close()
+
+print filename + " was converted."
---
+++ lab3/bitmap-generator/python implementation/run_for_all.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+import sys
+import glob
+import filecmp
+
+originFolder = "pbm"
+destinationFolder = "bit"
+program_name = "python converter.py"
+arguments = ""
+
+
+files = list(set(glob.glob(originFolder + "/*")) - set(glob.glob(originFolder + "/*.bit")) - set(glob.glob(originFolder + "/*.exclude")))
+prefix_lenght = len(originFolder + "/")
+
+for i, val in enumerate(files):
+	files[i] = files[i][prefix_lenght:]
+
+for filename in files:
+	os.system(program_name + " " + originFolder + "/" + filename + arguments)
+	shutil.move(originFolder + "/" + filename + ".bit", destinationFolder + "/" + filename + ".bit")
+
+print
+print program_name + " was ran for all files in the directory " + originFolder		
